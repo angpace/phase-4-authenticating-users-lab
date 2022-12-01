d8a549a896646fe09a3bab2bec310949da933feb
angpace <108838753+angpace@users.noreply.github.com>
2022-12-01 13:43:55 -0500

    commit tree b0d09f6f9e5472e5be8c3f3ffb1296cb84b3479a

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username])
+        session[:user_id] = user.id
+        render json: user
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+end
+
+
+
+
+# class UsersController < ApplicationController
+# def show
+#     user = User.find_by(id: session[:user_id])
+#     if user
+#       render json: user
+#     else
+#       render json: { error: "Not authorized" }, status: :unauthorized
+#     end
+#   end
+# end
